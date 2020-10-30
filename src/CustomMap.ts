7e7478a7e06bcb851430ad1220eb32adcd25e852
unsivil <unsivilaudio@gmail.com>
2020-10-30 10:49:03 -0600

    show markers on the map, update information in dom

---
+++ src/CustomMap.ts
@@ -0,0 +1,37 @@
+import User from './User';
+import Company from './Company';
+
+class CustomMap {
+    private googleMap: google.maps.Map;
+
+    constructor(element: Element) {
+        this.googleMap = new google.maps.Map(element, {
+            zoom: 1,
+            center: {
+                lat: 0,
+                lng: 0,
+            },
+        });
+    }
+
+    addUserMarker(user: User): void {
+        new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: user.location.lat,
+                lng: user.location.lng,
+            },
+        });
+    }
+    addCompanyMarker(company: Company): void {
+        new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: company.location.lat,
+                lng: company.location.lng,
+            },
+        });
+    }
+}
+
+export default CustomMap;
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,39 @@
 // Gmaps API KEY -- AIzaSyBNLrJhOMz6idD05pzfn5lhA-TAw-mAZCU
-
+import CustomMap from './CustomMap';
 import User from './User';
 import Company from './Company';
 
 import './assets/stylesheets/main.scss';
 
-// const user = new User();
-// const company = new Company();
+const mapEl = document.getElementById('map');
 
-// console.log(user);
-// console.log(company);
+const user = new User();
+const company = new Company();
+const map = new CustomMap(mapEl);
 
-const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 1,
-    center: {
-        lat: 0,
-        lng: 0,
-    },
-});
+map.addUserMarker(user);
+map.addCompanyMarker(company);
+updateUserFields(user);
+updateCompanyFields(company);
+
+function updateUserFields(user: User): void {
+    const name = document.getElementById('user-name');
+    const locLat = document.getElementById('user-lat');
+    const locLng = document.getElementById('user-lng');
+
+    name.textContent = user.name;
+    locLat.textContent = user.location.lat.toString();
+    locLng.textContent = user.location.lng.toString();
+}
+
+function updateCompanyFields(company: Company): void {
+    const name = document.getElementById('company-name');
+    const motto = document.getElementById('company-motto');
+    const locLat = document.getElementById('company-lat');
+    const locLng = document.getElementById('company-lng');
+
+    name.textContent = company.companyName;
+    motto.textContent = company.catchPhrase;
+    locLat.textContent = company.location.lat.toString();
+    locLng.textContent = company.location.lng.toString();
+}
